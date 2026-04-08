369783b5a11dbbd4e87d195032b05b6059f91a70
Jakub Tkaczuk <jtkaczuk@ethz.ch>
2026-04-08 22:46:45 +0200

    extract material dict to module-level, add available_materials(), improve error msg

--- SolidProps/SolidProps.py
+++ SolidProps/SolidProps.py
@@ -3,57 +3,70 @@ import pandas as pd
 from scipy import interpolate
 
 
+MATERIAL_FILES = {
+    'AL5083': 'ALUMINUM_ALLOY_5083-T0.csv',
+    'AL6061': 'ALUMINUM_ALLOY_6061-T6.csv',
+    'AL': 'ALUMINUM_PURE.csv',
+    'ALUMINUM': 'ALUMINUM_PURE.csv',
+    'AU': 'GOLD_PURE.csv',
+    'BRASS': 'COPPER_ZINC_90_10.csv',
+    'CARBON_FIBER_NORMAL': 'CARBON_FIBER_NORMAL_TO_FIBER.csv',
+    'CARBON_FIBER_PARALLEL': 'CARBON_FIBER_PARALLEL.csv',
+    'COPPER': 'COPPER_PURE.csv',
+    'COPPER-NICKEL_57-43': 'COPPER-NICKEL_57-43.csv',
+    'COPPER-NICKEL_70-30': 'COPPER-NICKEL_70-30.csv',
+    'COPPER-NICKEL_90-10': 'COPPER-NICKEL_90-10.csv',
+    'COPPER-ZINC_90-10': 'COPPER_ZINC_90_10.csv',
+    'CU': 'COPPER_PURE.csv',
+    'CU-NI_57-43': 'COPPER-NICKEL_57-43.csv',
+    'CU-NI_70-30': 'COPPER-NICKEL_70-30.csv',
+    'CU-NI_90-10': 'COPPER-NICKEL_90-10.csv',
+    'CZ-ZN_90-10': 'COPPER_ZINC_90_10.csv',
+    'CUPRONICKEL_57-43': 'COPPER-NICKEL_57-43.csv',
+    'CUPRONICKEL_70-30': 'COPPER-NICKEL_70-30.csv',
+    'CUPRONICKEL_90-10': 'COPPER-NICKEL_90-10.csv',
+    'EPOXY': 'EPOXY.csv',
+    'G10_NORMAL_TO_CLOTH': 'G10_NORMAL_TO_CLOTH.csv',
+    'G10_PARALLEL_TO_FILL': 'G10_PARALLEL_TO_FILL.csv',
+    'G10_PARALLEL_TO_WRAP': 'G10_PARALLEL_TO_WRAP.csv',
+    'GOLD': 'GOLD_PURE.csv',
+    'INVAR': 'INVAR-36.csv',
+    'INVAR-36': 'INVAR-36.csv',
+    'LEAD': 'LEAD_PURE.csv',
+    'KAPTON': 'KAPTON.csv',
+    'PB': 'LEAD_PURE.csv',
+    'SAPPHIRE': 'SAPPHIRE_PURE.csv',
+    'SS304L': 'STAINLESS_STEEL_304L.csv',
+    'SS310L': 'STAINLESS_STEEL_310L.csv',
+    'TI': 'TITANIUM_PURE.csv',
+    'TITANIUM': 'TITANIUM_PURE.csv',
+}
+
+
+def available_materials():
+    """ return a sorted list of available material names """
+    return sorted(MATERIAL_FILES.keys())
+
+
 class SolidProps(object):
     def __init__(self, material):
         """ class for interfacing material properties of solids """
         self._set_df(material)
+        self.material = material.upper()
         self.interp_K = self._get_interpolator('K/(W/(m.K))')
         self.interp_cp = self._get_interpolator('cp/(J/(kg.K))')
         self.interp_alpha_th = self._get_interpolator('alpha_thermal/(1/K)')
         self.interp_elec_resistivity = self._get_interpolator('resistivity/(ohm.m)')
 
     def _set_df(self, material):
-        d = {'AL5083': 'ALUMINUM_ALLOY_5083-T0.csv',
-             'AL6061': 'ALUMINUM_ALLOY_6061-T6.csv',
-             'AL': 'ALUMINUM_PURE.csv',
-             'ALUMINUM': 'ALUMINUM_PURE.csv',
-             'AU': 'GOLD_PURE.csv',
-             'BRASS': 'COPPER_ZINC_90_10.csv',
-             'CARBON_FIBER_NORMAL': 'CARBON_FIBER_NORMAL_TO_FIBER.csv',
-             'CARBON_FIBER_PARALLEL': 'CARBON_FIBER_PARALLEL.csv',
-             'COPPER': 'COPPER_PURE.csv',
-             'COPPER-NICKEL_57-43': 'COPPER-NICKEL_57-43.csv',
-             'COPPER-NICKEL_70-30': 'COPPER-NICKEL_70-30.csv',
-             'COPPER-NICKEL_90-10': 'COPPER-NICKEL_90-10.csv',
-             'COPPER-ZINC_90-10': 'COPPER_ZINC_90_10.csv',
-             'CU': 'COPPER_PURE.csv',
-             'CU-NI_57-43': 'COPPER-NICKEL_57-43.csv',
-             'CU-NI_70-30': 'COPPER-NICKEL_70-30.csv',
-             'CU-NI_90-10': 'COPPER-NICKEL_90-10.csv',
-             'CZ-ZN_90-10': 'COPPER_ZINC_90_10.csv',
-             'CUPRONICKEL_57-43': 'COPPER-NICKEL_57-43.csv',
-             'CUPRONICKEL_70-30': 'COPPER-NICKEL_70-30.csv',
-             'CUPRONICKEL_90-10': 'COPPER-NICKEL_90-10.csv',
-             'EPOXY': 'EPOXY.csv',
-             'G10_NORMAL_TO_CLOTH': 'G10_NORMAL_TO_CLOTH.csv',
-             'G10_PARALLEL_TO_FILL': 'G10_PARALLEL_TO_FILL.csv',
-             'G10_PARALLEL_TO_WRAP': 'G10_PARALLEL_TO_WRAP.csv',
-             'GOLD': 'GOLD_PURE.csv',
-             'INVAR': 'INVAR-36.csv',
-             'INVAR-36': 'INVAR-36.csv',
-             'LEAD': 'LEAD_PURE.csv',
-             'KAPTON': 'KAPTON.csv',
-             'PB': 'LEAD_PURE.csv',
-             'SAPPHIRE': 'SAPPHIRE_PURE.csv',
-             'SS304L': 'STAINLESS_STEEL_304L.csv',
-             'SS310L': 'STAINLESS_STEEL_310L.csv',
-             'TI': 'TITANIUM_PURE.csv',
-             'TITANIUM': 'TITANIUM_PURE.csv'}
-        if material.upper() not in d.keys():
-            raise TypeError(f'Unknown properties of {material}')
+        if material.upper() not in MATERIAL_FILES:
+            raise ValueError(
+                f"Unknown material '{material}'. "
+                f"Use available_materials() to list supported materials."
+            )
         location = os.path.dirname(os.path.realpath(__file__))
-        data = os.path.join(location, 'lib', d[material.upper()])
-        self.df = pd.read_csv(data, sep = r',', engine='python')
+        data = os.path.join(location, 'lib', MATERIAL_FILES[material.upper()])
+        self.df = pd.read_csv(data, sep=r',', engine='python')
 
     def _get_interpolator(self, prop):
         return interpolate.interp1d(self.df['T/K'], self.df[prop])
@@ -62,9 +75,8 @@ class SolidProps(object):
         assert T >= 1.0, f"Temperature {T} K below the 1 K limit"
         assert T <= 300.0, f"Temperature {T} K above the 300 K limit"
 
-    def get_rhomass(self, T=200):
+    def get_rhomass(self):
         """ return density in (kg/m^3) """
-        self.check_T_in_range(T)
         return float(self.df['rho/(kg/m3)'][0])
 
     def get_K(self, T):
@@ -80,7 +92,7 @@ class SolidProps(object):
     def get_cv(self, T):
         """
         return Cv in (J/(kg.K))
-        for incrompressible substances Cp = Cv
+        for incompressible substances Cp = Cv
         """
         return self.get_cp(T)
 
--- SolidProps/__init__.py
+++ SolidProps/__init__.py
@@ -1,1 +1,38 @@
-from .SolidProps import SolidProps
+from .SolidProps import SolidProps, available_materials
+
+__version__ = '1.1'
+
+
+def get_K(material, T):
+    """ return thermal conductivity in (W/(m.K)) for given material and temperature """
+    return SolidProps(material).get_K(T)
+
+
+def get_cp(material, T):
+    """ return specific heat in (J/(kg.K)) for given material and temperature """
+    return SolidProps(material).get_cp(T)
+
+
+def get_cv(material, T):
+    """ return Cv in (J/(kg.K)) for given material and temperature """
+    return SolidProps(material).get_cv(T)
+
+
+def get_rhomass(material):
+    """ return density in (kg/m^3) for given material """
+    return SolidProps(material).get_rhomass()
+
+
+def get_thermal_expansion_coefficient(material, T):
+    """ return thermal expansion coefficient in (1/K) for given material and temperature """
+    return SolidProps(material).get_thermal_expansion_coefficient(T)
+
+
+def get_thermal_diffusivity(material, T):
+    """ return thermal diffusivity in (m^2/s) for given material and temperature """
+    return SolidProps(material).get_thermal_diffusivity(T)
+
+
+def get_electrical_resistivity(material, T):
+    """ return electrical resistivity in (ohm.m) for given material and temperature """
+    return SolidProps(material).get_electrical_resistivity(T)
